c56c333907e735f13fce66a414209bc50f2642ab
ErzaKurtaj <erzzaakurtaj@gmail.com>
2024-08-01 13:48:51 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+
+import eventImage from './assets/images/eventImage.png';
+import componentsImage from './assets/images/componentsImage.jpg';
+import stateImage from './assets/images/stateImage.png';
+
+const concepts = [
+  {
+    title: 'Components',
+    image: componentsImage,
+    description: 'Components are the fundamental building blocks of a React application. They are JavaScript functions or classes that optionally accept inputs (known as "props") and return React elements that describe how a section of the UI should appear.'
+  },
+  {
+    title: 'State',
+    image: stateImage,
+    description: 'State is data that may change over the lifetime of a component. It is managed within the component (or possibly its children) and can be updated by calling the setState method.'
+  },
+  {
+    title: 'Events',
+    image: eventImage,
+    description: 'Event handlers are added to React elements to respond to user interactions, such as clicks, form submissions, or mouse movements.'
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul id="concepts"></ul>
+      <li className="concept">
+      <img src={concepts[0].image} alt={concepts[0].title}></img>
+      <h2>{concepts[0].title}</h2>
+      <p>{concepts[0].description}</p>
+      </li>
+      <li className="concept">
+      <img src={concepts[1].image} alt={concepts[1].title}></img>
+      <h2>{concepts[1].title}</h2>
+      <p>{concepts[1].description}</p>
+      </li>
+      <li className="concept">
+      <img src={concepts[2].image} alt={concepts[2].title}></img>
+      <h2>{concepts[2].title}</h2>
+      <p>{concepts[2].description}</p>
+      </li>
     </div>
+    
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import keyconceptsImage from '../assets/images/keyconceptsImage.png';
+
+function Header(){
+return
+<header>
+<img src={keyconceptsImage} alt="Metal badge with a star"/>
+<h1>Key concepts Image</h1>
+<p>Selected key React concepts you should know</p>
+</header>
+}
+
+export default Header;
